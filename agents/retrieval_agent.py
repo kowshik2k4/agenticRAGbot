057522de71d39eece3bc3c4b2cd0e46046d79c6f
kowshik2k4 <133842990+kowshik2k4@users.noreply.github.com>
2025-07-22 13:13:13 +0530

    Create retrieval_agent.py

---
+++ agents/retrieval_agent.py
@@ -0,0 +1,33 @@
+from sentence_transformers import SentenceTransformer
+import faiss
+from mcp.message import create_message
+import numpy as np
+
+class RetrievalAgent:
+    def __init__(self):
+        self.model = SentenceTransformer("all-MiniLM-L6-v2")
+        self.index = faiss.IndexFlatL2(384)
+        self.chunks = []
+
+    def store_chunks(self, chunks):
+        embeddings = self.model.encode(chunks)
+        self.index.add(np.array(embeddings))
+        self.chunks.extend(chunks)
+
+    def retrieve(self, query):
+        q_embedding = self.model.encode([query])
+        D, I = self.index.search(np.array(q_embedding), 5)
+        results = [self.chunks[i] for i in I[0] if i < len(self.chunks)]
+        return results
+
+    def run(self, message):
+        if message["type"] == "INGESTION_COMPLETE":
+            self.store_chunks(message["payload"]["chunks"])
+        elif message["type"] == "QUERY":
+            top_chunks = self.retrieve(message["payload"]["query"])
+            return create_message(
+                sender="RetrievalAgent",
+                receiver="LLMResponseAgent",
+                type_="RETRIEVAL_RESULT",
+                payload={"retrieved_context": top_chunks, "query": message["payload"]["query"]}
+            )
