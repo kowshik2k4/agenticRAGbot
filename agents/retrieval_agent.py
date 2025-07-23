382ba18cc92bb8ceb5fc41071b2655a63eeb13e2
kowshik2k4 <133842990+kowshik2k4@users.noreply.github.com>
2025-07-23 13:02:58 +0530

    Update retrieval_agent.py

--- agents/retrieval_agent.py
+++ agents/retrieval_agent.py
@@ -2,10 +2,10 @@ from sentence_transformers import SentenceTransformer
 import faiss
 from mcp.message import create_message
 import numpy as np
-
+  from torch import device
 class RetrievalAgent:
     def __init__(self):
-        self.model = SentenceTransformer("all-MiniLM-L6-v2")
+        self.model = SentenceTransformer("all-MiniLM-L6-v2", device="cuda" if torch.cuda.is_available() else "cpu")
         self.index = faiss.IndexFlatL2(384)
         self.chunks = []
 
